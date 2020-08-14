6b0e01a1aeb280429dc6c6fb54bd28d52a3ec9e8
Michael Ushakov <um.nix.user@gmail.com>
2020-08-14 17:05:17 +0500

    omot empty were applied to props

--- ASL/language/task.go
+++ ASL/language/task.go
@@ -17,25 +17,23 @@ const(
 	Map = "Map"
 )
 
-// Fat Task State (everything that could be stored
+// Fat Task Data that could accumulate data from any state (everything that could be stored
 type TaskData struct {
-    Comment string
+    Comment string `json:",omitempty"`
     Type TaskType
-    Resource string
-    Next string
-    End bool
+    Resource string `json:",omitempty"`
+    Next string `json:",omitempty"`
+    End bool `json:",omitempty"`
 
-    Retry Retry
-    Catch Catch
-
-
+    Retry Retry `json:",omitempty"`
+    Catch Catch `json:",omitempty"`
 
     // Data Related to Lambda Execution
     // Input path is a JSON from raw JSON that task process and pass to lambda
-    InputPath interface{}
+    InputPath interface{} `json:",omitempty"`
 	// Output path is a JSON from raw JSON that task gets from Lambda
-    OutputPath interface{}
-    ResultPath interface{}
-    Parameters interface{}
+    OutputPath interface{} `json:",omitempty"`
+    ResultPath interface{} `json:",omitempty"`
+    Parameters interface{} `json:",omitempty"`
 }
 
